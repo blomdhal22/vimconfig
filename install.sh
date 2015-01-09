8fedd74087a4c16fad83b663879b12d9a4d5012a
Blomdhal <shoon95@gmail.com>
2015-01-09 20:26:16 +0900

    2015-01-09, Apply Update: Bundle to Vundle

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 if [ -e ~/.vimrc -o -e ~/.vim ]; then
-	echo "경고: 설치를 진행하려면 ~/.vim/ 디렉토리와 ~/.vimrc 파일을 삭제해야 합니다."
+	echo "Error!: Please Remove '~/.vim/' directory and  '~/.vimrc' file."
 	exit
 fi
 
-echo "심볼릭 릭크 생성..."
-ln -sfv ~/vimconfig/vimrc ~/.vimrc
+echo "Copy vimrc to '~/.vimrc'..."
+#ln -sfv ~/vimconfig/vimrc ~/.vimrc
+cp ~/vimconfig/vimrc ~/.vimrc
 
-echo "vundle 다운로드중..."
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#echo "vundle 다운로드중..."
+echo "Downloading vundle..."
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "vundle 설치중..."
-vi -c :BundleInstall -c :qa
+echo "Install vundle..."
+vim +PluginInstall +qall
+echo "Install completed."
